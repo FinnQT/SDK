403ec5d6bbfbc74c8810ba0cc0acfc57208539bf
chu quyet thang <quyetthang61271@gmail.com>
2024-05-26 22:38:11 +0700

    fix excharge rate

--- resources/views/webpay/exchangerate.blade.php
+++ resources/views/webpay/exchangerate.blade.php
@@ -9,16 +9,15 @@
         <!-- Main content -->
         <div class="content">
             <div class="container-fluid ">
-                <div class="row  d-flex justify-content-center">
-                    <div class="col mt-2">
+                <div class="row">
+                    <div class="col-12">
                         <div class="card">
                             <div class="card-header">
                                 <h3 class="card-title">Tỉ giá cào - nạp thẻ</h3>
                             </div>
                             <!-- /.card-header -->
-                            <div class="card-body">
-                                <div class="alert alert-danger" id="messages-fail" style="display: none;"></div>
-                                <table class="table table-bordered text-center">
+                            <div class="card-body table-responsive p-0">
+                                <table class="table table-bordered text-nowrap text-center">
                                     <thead>
                                         <tr>
                                             <th>Loại Thẻ</th>
@@ -37,43 +36,37 @@
                                     <tbody>
                                         <tr>
                                             <th>Tỉ giá</th>
-                                            <td>{{ $data->rate*100 }}%</td>
-                                            <td>{{ $data->rate*100 }}%</td>
-                                            <td>{{ $data->rate*100 }}%</td>
-                                            <td>{{ $data->rate*100 }}%</td>
-                                            <td>{{ $data->rate*100 }}%</td>
-                                            <td>{{ $data->rate*100 }}%</td>
-                                            <td>{{ $data->rate*100 }}%</td>
-                                            <td>{{ $data->rate*100 }}%</td>
-                                            <td>{{ $data->rate*100 }}%</td>
-                                            <td>{{ $data->rate*100 }}%</td>
+                                            <td>{{ $data->rate * 100 }}%</td>
+                                            <td>{{ $data->rate * 100 }}%</td>
+                                            <td>{{ $data->rate * 100 }}%</td>
+                                            <td>{{ $data->rate * 100 }}%</td>
+                                            <td>{{ $data->rate * 100 }}%</td>
+                                            <td>{{ $data->rate * 100 }}%</td>
+                                            <td>{{ $data->rate * 100 }}%</td>
+                                            <td>{{ $data->rate * 100 }}%</td>
+                                            <td>{{ $data->rate * 100 }}%</td>
+                                            <td>{{ $data->rate * 100 }}%</td>
                                         </tr>
                                         <tr>
                                             <th>Thực Nhận</th>
-                                           <td>{{10000 - $data->rate*10000 }} xu</td>     
-                                           <td>{{20000 - $data->rate*20000 }} xu</td>
-                                           <td>{{50000 - $data->rate*50000 }} xu</td>
-                                           <td>{{100000 - $data->rate*100000 }} xu</td>
-                                           <td>{{200000 - $data->rate*200000 }} xu</td>
-                                           <td>{{500000 - $data->rate*500000 }} xu</td>
-                                           <td>{{1000000 - $data->rate*1000000 }} xu</td>
-                                           <td>{{2000000 - $data->rate*2000000 }} xu</td>
-                                           <td>{{3000000 - $data->rate*3000000 }} xu</td>
-                                           <td>{{5000000 - $data->rate*5000000 }} xu</td>
+                                            <td>{{ 10000 - $data->rate * 10000 }} xu</td>
+                                            <td>{{ 20000 - $data->rate * 20000 }} xu</td>
+                                            <td>{{ 50000 - $data->rate * 50000 }} xu</td>
+                                            <td>{{ 100000 - $data->rate * 100000 }} xu</td>
+                                            <td>{{ 200000 - $data->rate * 200000 }} xu</td>
+                                            <td>{{ 500000 - $data->rate * 500000 }} xu</td>
+                                            <td>{{ 1000000 - $data->rate * 1000000 }} xu</td>
+                                            <td>{{ 2000000 - $data->rate * 2000000 }} xu</td>
+                                            <td>{{ 3000000 - $data->rate * 3000000 }} xu</td>
+                                            <td>{{ 5000000 - $data->rate * 5000000 }} xu</td>
                                         </tr>
-
                                     </tbody>
                                 </table>
                             </div>
                             <!-- /.card-body -->
-                            <div class="card-footer clearfix">
-                            </div>
-
-
                         </div>
                         <!-- /.card -->
                     </div>
-                    <!-- /.col -->
                 </div>
             </div>
             <!-- /.container-fluid -->
